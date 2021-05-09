2ef5caacfb99ff58f796a6ab88a632b51d50ffeb
Edmilson Andrade <edmilson.andrad@gmail.com>
2021-05-09 20:00:34 -0300

    Margin Home Page

--- frontend/src/Page/Home/index.tsx
+++ frontend/src/Page/Home/index.tsx
@@ -10,7 +10,7 @@ function Home() {
                 <div className="d-flex flex-column justify-content-center">
                     <h1 className="display-4">DSVendas</h1>
                     <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
-                    <hr />
+                    <hr/>
                     <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
                     <Link className="btn btn-primary btn-lg" to="/dashboard">
                         Acessar Dashboard
